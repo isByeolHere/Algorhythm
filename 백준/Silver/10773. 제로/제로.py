cc8da45cbc8d00d990f78fcb65445c8272a376ce
김별 <167271769+isByeolHere@users.noreply.github.com>
2025-03-22 21:30:12 +0900

    [Silver IV] Title: 제로, Time: 72 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,15 @@
+import sys 
+
+k = int(sys.stdin.readline())
+stack = []
+
+for i in range(k):
+
+  a = int(sys.stdin.readline())
+
+  if a == 0:
+    stack.pop()
+  else:
+    stack.append(a)
+
+print(sum(stack))
