e9c8404b010d0009b5d23eee4c3a190e79ae752e
김별 <167271769+isByeolHere@users.noreply.github.com>
2025-03-23 17:41:16 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,22 +1,28 @@
-N = int(input())  # 입력 배열의 크기
-arr = list(map(int, input().split()))  # 입력 배열
 
-## 이진탐색 함수
-def binary_search(arr, target):
-    left, right = 0, len(arr) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if arr[mid] < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-    return left
+import sys
 
-lis = [arr[0]]
-for i in range(1, len(arr)):
-    if arr[i] > lis[-1]:
-        lis.append(arr[i])
+n = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))
+
+def bin_search(arr, target):
+  start = 0
+  end = len(arr) - 1
+  while start <= end:
+    mid = (start+end) // 2
+
+    if arr[mid] < target:
+      start = mid + 1
+
     else:
-        index = binary_search(lis, arr[i])
-        lis[index] = arr[i]
+      end = mid - 1
+  return start
+
+lis = [a[0]]
+for i in range(1, len(a)):
+  if a[i] > lis[-1]:
+    lis.append(a[i])
+  else:
+    new_index = bin_search(lis, a[i])
+    lis[new_index] = a[i]
+
 print(len(lis))
