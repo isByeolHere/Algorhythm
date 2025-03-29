9d19134af5811a54e6fac548ba51204caae95db1
김별 <167271769+isByeolHere@users.noreply.github.com>
2025-03-29 14:54:17 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,20 +1,14 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 def bfs(start):
-  queue = deque([start])
   visited[start] = True
+  for i in graph[start]:
+    if not visited[i]:
+      bfs(i)
 
-  while queue:
-    v = queue.popleft()
-    for i in graph[v]:
-      if not visited[i]:
-        queue.append(i)
-        visited[i] = True
-
-com = int(input().strip())
-edge = int(input().strip())
+com = int(input())
+edge = int(input())
 graph = [[] for _ in range(com+1)]
 visited = [False] * (com+1)
 
@@ -22,7 +16,6 @@ for i in range(edge):
   a, b = map(int, input().split())
   graph[a].append(b)
   graph[b].append(a)
-
 
 bfs(1)
 print(visited.count(True)-1)
