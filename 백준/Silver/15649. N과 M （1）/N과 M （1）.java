20522a11773d03c7fdf57d1c2102b874a6d3fa76
Byeol <167271769+isByeolHere@users.noreply.github.com>
2025-08-21 20:47:53 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 22888 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,45 @@
+//  N과 M (1)
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    static int n, m;
+    static boolean[] visited;
+    static int[] pick;
+    static StringBuilder out = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[n+1];
+        pick = new int[m];
+
+        dfs(0);
+        System.out.print(out.toString());
+
+
+        }
+
+    static void dfs(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < m; i++) {
+                out.append(pick[i]).append(' ');
+            }
+            out.append('\n');
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            pick[depth] = i;
+            dfs(depth + 1);
+            visited[i] = false;
+        }
+      }
+    }
